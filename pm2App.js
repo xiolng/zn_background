42f1d7a1f6ddc0d50544a18b9e4e8564e5ed94c9
xiolng <zheng_zhanning@126.com>
2019-02-21 15:20:16 +0800

    pm2AppJson

--- pm2App.js
+++ pm2App.js
@@ -11,7 +11,7 @@ module.exports = {
             "node_modules",
             "logs"
         ],
-        "exec_mode": "fork",                // 应用启动模式，支持fork和cluster模式
+        "exec_mode": "cluster",                // 应用启动模式，支持fork和cluster模式
         "instances": 0,                             // 应用启动实例个数，仅在cluster模式有效 默认为fork；或者 max
         "max_memory_restart": "300M",                    // 最大内存限制数，超出自动重启
         "error_file": "./logs/app-err.log",         // 错误日志文件
